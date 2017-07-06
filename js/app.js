63bc717c55b75d26182c6752e676d685c3a635a5
mounga <christopher.fourgeaud@gmail.com>
2017-07-06 11:53:23 +0200

    3éme et 4éme exercice terminé.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 //Step 2* : rajouter "Hello world" dans le paragraphe d'id 'addText';
 $("#addText")
 .html("HELLOOOOOOOOO");
+
+// - *Step 3* : supprimer la div d'id 'remove';
+$("#remove").remove();
+
+// - *Step 4* : changer la couleur de background de la div d'id 'changeColor';
+$("#changeColor").css('background-color','red');
+
+// - *Step 5* : ajouter 4 balises 'p' dans la div d'id 'addChildren';
